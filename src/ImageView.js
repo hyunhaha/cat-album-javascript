21cd29c336947b18869baf166c27b6ecfa143285
hyunhaha <gusgk2658@naver.com>
2021-09-03 15:41:26 +0900

    이미지 모달 창 닫기 기능

--- src/ImageView.js
+++ src/ImageView.js
@@ -1,4 +1,5 @@
 const IMAGE_PATH_PREFIX = 'https://fe-dev-matching-2021-03-serverlessdeploymentbuck-t3kpj3way537.s3.ap-northeast-2.amazonaws.com/public'
+
 export default function ImageView({ $app, initialState }) {
   this.state = initialState;
 
@@ -11,13 +12,32 @@ export default function ImageView({ $app, initialState }) {
     this.state = nextState;
     this.render()
   }
+  this.onclose = (e) => {
 
+
+  }
   this.render = () => {
     this.$target.innerHTML = `<div class="content">
     ${this.state ? `<img src="${IMAGE_PATH_PREFIX}${this.state}">` : ''}
     </div>`;
 
     this.$target.style.display = this.state ? 'block' : 'none';
+
+    if (this.$target.style.display === 'block') {
+      this.$target.addEventListener('click', (e) => {
+        if (e.target === e.currentTarget) {
+          this.$target.style.display = 'none'
+        }
+      });
+
+      window.addEventListener('keydown', (event) => {
+        event.stopImmediatePropagation()
+        if (event.key === "Escape") {
+          this.$target.style.display = 'none'
+        }
+      });
+    }
+
   }
   this.render();
 }
